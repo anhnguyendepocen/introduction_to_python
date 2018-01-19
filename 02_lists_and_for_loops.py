1eee2ab92b5e17b097d2bd9d4f00354330768414
ckelling <ckelling@vt.edu>
2018-01-18 21:00:17 -0500

    going through the for loop and list training

--- 02_lists_and_for_loops.py
+++ 02_lists_and_for_loops.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Introduction to Python 2
+Introduction to Python, 2
 Created on Thu Jan 18 20:53:02 2018
 
 @author: Claire Kelling
@@ -8,226 +8,112 @@ Created on Thu Jan 18 20:53:02 2018
 The purpose of this file is to learn how to create lists and for loops.
 """
 
-Input/Eval Input
-
-c = input(‘Enter a number: ‘)
-
+# Input/Eval (evaluate) Input
+# input asks the user to enter something into the console, 
+#     and stores it as a variable
+# type says int, string, list, or float
+# a float is a double precision number (not an integer)
+c = input("Enter a number: ")
 type(c)
 
-d = eval(input(‘Enter a number: ‘))
+# eval tries to convert a string into a number
+# now this variable is an int, instead of a string
+d = eval(input("Enter a number: "))
 
+##################
+### Data Types
+##################
 type(d)
 type('3')
 type(3)
 type(3.1)
+# int converts a float to an integer (essentially rounds it)
+# if you do a round on a regular float, it stays a float
 int(3.1)
+#converts an integer to a float
 float(3)
 
-Lists/indexing lists
-
+##################
+### Lists/indexing lists
+##################
+# creating a list, similar to a vector in R
 q = [1, 2, 3, 4]
-len(q)
-q[1]
+len(q) #length of q
+# *** Python indices start at 0
+# the last element is indice (length of list) - 1
+q[1] 
 q[0]
 q[3]
 q[4]
+
+# you can access the last of an array using -1
 q[-1]
 q[-2]
 q[-4]
-q[-5]
+q[-5] # out of range
+
+#to look at a range of values
 q[0:2]
-q[0:4]
-q[0:4:2]
-q[0::2]
-q[::2]
+q[0:4] # the second index "up until" so including elements 0,1,2,3
+q[0:4:2] # 0th element to 4th (but not including) in increments of 2
+q[0::2] # this doesn't have an end, so gets every second element until end of list
+q[::2] # doesn't have beginning, so gets every second element
 
 w = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 w[0::3]
 w[2::3]
 
+# empty list
 z = []
-len(z)
-z.append(3)
+len(z) #length 0
+# Can add or remove values in a list
+z.append(3) # add single element
 z
 z.append(7)
 z
 z.remove(7)
+z
 z.append(1)
 z.append(1)
 z.append(3)
 z
-z.remove(1)
+z.remove(1) #removes the first value in the list
 z
-z.append(‘words’)
+z.append("words") # can also append different data types
 z
-z[0]
+z[0] # access values again
 z[1]
-z[1][2]
-str1[0:3]
+# sub index of the list element, can index strings like lists
+z[3][2] 
 
-For loops
+# can also have lists within lists
+a = [1, 2]
+b= [3, 4]
+c= [a, b] # list of lists
 
+##################
+### For Loops
+##################
+
+#creating a list
 myList = [2, 3, 5, 8, 13]
+
+# there are no brackets in Python
+# tabs keep track of the nesting structure
+# the following uses my list from above as the indexing variable
+#     You need a colon after the loop or conditional statement ***
+#     Also, you cannot run individual lines, must be run together
 for n in myList:
     print(n)
 
-for i in range(10):
+for i in range(10): #indexes from 0 to 9
     print(i)
-	
-for n in str1:
+    
+for j in range(len(myList)):
+    print(myList[j])
+    
+for n in str1: # can index through strings as well
     print(n)
 
 for n in range(len(str1)):
-    print(n, ':', str1[n])
-
-for j in range(len(
-
-Logical operations
-
-a == b
-a > b
-a >= b
-a != b
-a != b and a < b
-a != b or a < b
-True
-False
-True and False
-True and True
-False or True
-False and False
-
-str1
-‘h’ in str1
-‘q’ in str1
-‘q’ not in str1
-
-If statements
-
-if a == 2:
-    print(‘a is equal to 2’)
-
-if a == 2:
-    print(‘a is equal to 2’)
-else:
-    print(‘a is not equal to 2’)
-
-While statements
-
-while a < 10:
-    a = a + 1
-	print(a)
-
-a = 2
-
-while a < 10:
-    a += 1
-    print(a)
-
-a = 2
-
-while a < 25:
-    a *= 2
-    print(a)
-
-Order/characters
-
-ord('a')
-
-ord('b') 
-
-ord(‘A’) 
-
-chr(97)
-
-for n in range(50, 100):
-    print(chr(n))
-
-chr(ord(‘a’))
-chr(ord('a')+1)
-chr(ord('a')+10)
-
-alphabet = []
-m = 0
-while ‘z’ not in alphabet:
-alphabet.append(chr(ord('a') + m))
-	m += 1
-	print(alphabet)
-
-'z' in alphabet
-'Z' in alphabet
-'Z'.lower() in alphabet
-
-Function definitions, inputs, and outputs
-
-def main():
-    # do something
-	print(‘Hello world’) # btw this is a comment
-
-main()
-
-def timesTwo(x):
-    return 2*x
-
-timesTwo(10)
-
-def sumThreeNums(m, n, p):
-	return m+n+p
-
-sumNumbers(1, 2, 3)
-
-def squareRoot(x):
-	import math
-	root1 = math.sqrt(x)
-	root2 = -math.sqrt(x)
-	return root1, root2
-
-a, b = squareRoot(9)
-a
-b
-
-squareRoot(sumNumbers(2, 3, 4))
-
-PEP8 online check
-
-http://pep8online.com/
-
-Read simple files (1D list)
-
-fname = ‘acc2.dat’
-infile = open(fname, "r")
-longStr = infile.read()
-data = longStr.split(‘\n’)
-infile.close() # VERY IMPORTANT 
-
-len(data)
-max(data)
-min(data)
-
-import statistics as stat
-stat.mean(data)
-
-Why the error? Need to convert to list of floats instead of strings
-
-data2 = list(map(float, data))
-data2
-
-or use LIST COMPREHENSION (useful concept)
-data3 = [float(i) for i in data]
-data3
-
-data2 – data3  # should be zero, right?
-
-result = [data2[i] - data3[i] for i in range(len(data2))]
-
-
-
-Plotting with MatPlotLib
-
-import matplotlib.pyplot as plt
-plt.plot(data)
-
-import numpy as np
-plt.plot(range(100, 200), data)
-
-plt.plot(range(0, 500, 10), data)
+    print(n, ':', str1[n]) # this uses 0 to 4 indexing, instead of through a string
