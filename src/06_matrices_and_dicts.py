1e0d889b051eb4ca4a980f17b6ed46a08f70f5b7
ckelling <ckelling@vt.edu>
2018-01-23 20:04:18 -0500

    initializing matrices and dicts

---
+++ src/06_matrices_and_dicts.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Introduction to Python, 6
+
+Created on Sat Jan 23 20:57:01 2018
+
+@author: Claire Kelling
+
+The purpose of this file is to learn matrices and dictionaries.
+
+"""
+
+#set working directory
+import os
+os.chdir('C:/Users/ckell/OneDrive/Penn State/Research/Python/introduction_to_python')
